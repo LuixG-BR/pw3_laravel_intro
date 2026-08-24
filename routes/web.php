dbbd83d7dbe83ad40779b2e844fa25e6bd92726a
Luis Gabriel <luisgabriel19396@gmail.com>
2026-08-24 14:47:07 -0300

    feat:(routes) intro ao eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Models\User;
 use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,13 @@ Route::get('/', function () {
 
 Route::get('/produtos', [ProdutoController::class, 'index']);
 Route::post('/produtos', [ProdutoController::class, 'store']);
+
+Route::get('/teste-orm', function() {
+    User::create([
+        'name' => 'Ana Clara Santos',
+        'email' => 'ana.santos@escola.sp.gov.br',
+        'password' => '12345678'
+    ]);
+
+    return User::all();
+});
